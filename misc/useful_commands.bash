d9e44e2390bb654a307a032e608c218fe334a253
Roshan Gonsalkorale <roshan.gonsalkorale@oracle.com>
2022-05-24 11:27:57 +0100

    Fixing Fake Pay via DB export
    -

---
+++ misc/useful_commands.bash
@@ -0,0 +1,42 @@
+
+
+# Getting DB
+
+## SSH into instance
+ssh -i YOURKEY_W_PATH opc@[INSTANCE_IP]
+
+## Get DB username/password
+docker exec opc_db_1 env
+
+## SSH into docker container and grab stuf
+docker exec -it opc_db_1 /bin/bash
+
+## Dump DB
+mysqldump -uwp -p'QBThK5Z5h_Ykkgr5' wordpress > db-backup.sql --no-tablespaces
+exit
+
+## Copy to instance
+docker cp opc_db_1:db-backup.sql .
+
+
+# Getting wp content
+
+## SSH into docker container and grab stuff
+docker exec -it opc_wordpress_1 /bin/bash
+
+## Zip content
+tar cvzf wp-content.tar.gz wp-content
+exit
+
+## Copy to instance
+docker cp opc_wordpress_1:wp-content.tar.gz .
+
+# Exit and copy to local machine
+exit
+scp -i YOURKEY_W_PATH opc@[INSTANCE_IP]:wp-content.tar.gz PATH_ON_LOCAL_MACHINE
+scp -i YOURKEY_W_PATH opc@[INSTANCE_IP]:db-backup.sql PATH_ON_LOCAL_MACHINE
+
+# DONE!
+
+result=`echo "http://12345" | sed "s/http/abc/g"`
+echo $result
